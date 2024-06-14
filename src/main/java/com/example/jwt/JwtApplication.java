4ac8fac2893e2e72ff6156957a88b3c861ef3d50
tron0318 <tron0318@naver.com>
2024-06-14 14:19:36 +0900

    init data

--- src/main/java/com/example/jwt/JwtApplication.java
+++ src/main/java/com/example/jwt/JwtApplication.java
@@ -2,8 +2,10 @@ package com.example.jwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing  // @CreatedDate, @LastModifiedDate를 사용하기 위해 필요
 public class JwtApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/jwt/domain/member/controller/MemberController.java
@@ -0,0 +1,30 @@
+package com.example.jwt.domain.member.controller;
+
+import com.example.jwt.domain.member.entity.Member;
+import com.example.jwt.domain.member.service.MemberService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/member")
+public class MemberController {
+    private final MemberService memberService;
+
+    @Data
+    public static class LoginRequest {
+        @NotBlank
+        private String username;
+
+        @NotBlank
+        private String password;
+    }
+
+    @PostMapping("/login")
+    public Member login(@Valid @RequestBody LoginRequest loginRequest) {
+        return memberService.findByUsername(loginRequest.getUsername()).orElse(null);
+    }
+}
